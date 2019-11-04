5e11737cc718584b448e6fe2098619338906cea5
angcastillo18 <angcastillo18@gmail.com>
2019-11-04 03:51:16 -0500

    preparando para Heroku ya

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports={
     port:process.env.PORT || 3001,
-    db:process.env.MONGODB_URI || 'mongodb+srv://angcastillo18:T3s1sB1k42019@angelodatabase-csg5n.mongodb.net/gameTesis?retryWrites=true&w=majority' 
+    db:process.env.MONGODB_URI || 'mongodb://localhost:27017/gameTesis' 
     //LOCAL.db:process.env.MONGODB || 'mongodb://localhost:27017/gameTesis'
 }
